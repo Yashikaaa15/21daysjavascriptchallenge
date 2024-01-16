28a0c57624ba4ba5a0104f867c9f7b25dd0d95c7
Yashikaaa15 <155467223+Yashikaaa15@users.noreply.github.com>
2024-01-16 15:54:49 +0530

    Create Day2.js

---
+++ Day2.js
@@ -0,0 +1,19 @@
+let a="Hello World"
+let e=",how are you?"
+let b=5
+let d=6
+let c=true
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+// adding,subtracting,multiplying,dividing a and b
+console.log(b+d)
+console.log(b-d)
+console.log(b*d)
+console.log(b/d)
+// The &&= Operator
+// If the first value is true, the second value is assigned
+let x=100;
+x &&=5;
+console.log("value of x is:"+ x)
+console.log(a.concat(e))

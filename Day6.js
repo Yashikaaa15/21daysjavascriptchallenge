28759ab46840b4a346aa3d0cfec54ddce5dd36f1
Yashikaaa15 <155467223+Yashikaaa15@users.noreply.github.com>
2024-01-16 15:59:43 +0530

    Create Day6.js

---
+++ Day6.js
@@ -0,0 +1,8 @@
+function calculateSum(number1, number2) {
+    var sum = number1 + number2;
+    return sum;
+}
+
+// Example usage:
+console.log( calculateSum(15, 25));
+console.log( calculateSum(1, 5));

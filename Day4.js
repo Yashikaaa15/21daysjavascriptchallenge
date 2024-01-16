48124642966dd9c34850abbf8abe95023b5cd73f
Yashikaaa15 <155467223+Yashikaaa15@users.noreply.github.com>
2024-01-16 15:58:00 +0530

    Create Day4.js

---
+++ Day4.js
@@ -0,0 +1,12 @@
+// printing 1 to 10 numbers using for loop
+console.log("using for loop")
+for(let i=0;i<=10;i++){
+console.log(i);
+}
+// printing 1 to 10 numbers using for loop
+console.log("using while loop")
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}

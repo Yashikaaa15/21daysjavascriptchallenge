7e45c3a84f9e57d94cb7f48b0420013e8c44fdcb
Yashikaaa15 <155467223+Yashikaaa15@users.noreply.github.com>
2024-01-16 16:00:34 +0530

    Create Day7.js

---
+++ Day7.js
@@ -0,0 +1,13 @@
+// day7.js
+
+// Define an object named person
+var person = {
+    name: "John Doe",
+    age: 25,
+    address: "123 Main Street, Cityville"
+  };
+  
+  // Access and display individual properties of the person object
+  console.log("Name: " + person.name);
+  console.log("Age: " + person.age);
+  console.log("Address: " + person.address);
